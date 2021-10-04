e8da9240a28f9649b67f6f1b44ae681538f230fb
Kerem Yalçın <66633262+Aegrandir@users.noreply.github.com>
2021-10-04 13:07:49 +0300

    Add files via upload

---
+++ Assets/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity;
+
+    private Transform parent;
+    private void Start()
+    {
+        parent = transform.parent;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    private void Update()
+    {
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+    }
+
+}
