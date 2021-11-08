e161a0efd15e2009a3d3f7ff5c7771145dcbfa0d
utkusever <c.utkusever@outlook.com>
2021-11-09 00:55:31 +0300

    Add AimCamScript&ShortAnim

--- Assets/Scripts/Core/CameraController.cs
+++ Assets/Scripts/Core/CameraController.cs
@@ -6,19 +6,38 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private float mouseSensitivity;
     [SerializeField] GameObject mainCam;
+    [SerializeField] GameObject thirdCam;
+    [SerializeField] GameObject aimCamera;
 
     private Transform parent;
     private void Start()
     {
         parent = transform.parent;
+        aimCamera.SetActive(false);
     }
     private void Update()
     {
         Rotate();
+        if (Input.GetMouseButton(1) && !aimCamera.activeInHierarchy)
+        {
+            thirdCam.SetActive(false);
+            aimCamera.SetActive(true);
+            print("holding right 1");
+
+        }
+        else if (!Input.GetMouseButton(1) && !thirdCam.activeInHierarchy)
+        {
+            thirdCam.SetActive(true);
+            aimCamera.SetActive(false);
+            print("released right 2");
+        }
     }
 
     private void Rotate()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
     }
+
+
+
 }
