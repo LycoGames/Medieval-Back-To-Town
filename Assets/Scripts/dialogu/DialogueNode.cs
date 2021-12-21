c6265a78cbd9d6626a4da039c0e42d311743f4df
utkusever <c.utkusever@outlook.com>
2021-12-21 18:30:10 +0300

    Add BasicEditor

---
+++ Assets/Scripts/dialogu/DialogueNode.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DialogueNode
+{
+    public string uniqueID;
+    public string text;
+    public string[] children;
+    public Rect rect = new Rect(0,0,200,100);
+   
+}
