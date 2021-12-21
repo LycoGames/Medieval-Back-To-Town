3a86d73f2cf352e959289cf6826362ec204b4e5d
ahmetsak-tr <ahmeterensak@outlook.com>
2021-12-22 01:27:34 +0300

    Add : Saving System

---
+++ Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using RPG.Saving;
+using UnityEngine;
+
+public class SavingWrapper : MonoBehaviour
+{
+    [SerializeField] KeyCode saveKey = KeyCode.K;
+    [SerializeField] KeyCode loadKey = KeyCode.L;
+    [SerializeField] KeyCode deleteKey = KeyCode.Delete;
+    const string defaultSaveFile = "save";
+
+    private void Awake()
+    {
+        StartCoroutine(LoadLastScene());
+    }
+
+    private IEnumerator LoadLastScene()
+    {
+        yield return GetComponent<SavingSystem>().LoadLastScene(defaultSaveFile);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(saveKey))
+            Save();
+        if (Input.GetKeyDown(loadKey))
+            Load();
+        if (Input.GetKeyDown(deleteKey))
+            Delete();
+    }
+
+    private void Save()
+    {
+        GetComponent<SavingSystem>().Save(defaultSaveFile);
+    }
+
+    private void Load()
+    {
+        StartCoroutine(GetComponent<SavingSystem>().LoadLastScene(defaultSaveFile));
+    }
+
+    private void Delete()
+    {
+        GetComponent<SavingSystem>().Delete(defaultSaveFile);
+    }
+}
