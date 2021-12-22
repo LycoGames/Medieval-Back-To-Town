5a708b46bfcf5bc293ea663a27dcb071ed491c7e
ahmetsak-tr <ahmeterensak@outlook.com>
2021-12-22 22:35:11 +0300

    Update: equipment ui inventory ui

--- Assets/Scripts/UI/Equipment/Equipment.cs
+++ Assets/Scripts/UI/Equipment/Equipment.cs
@@ -1,15 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using RPG.Saving;
 using UnityEngine;
 
-using RPG.Saving;
-/// <summary>
-/// Provides a store for the items equipped to a player. Items are stored by
-/// their equip locations.
-/// 
-/// This component should be placed on the GameObject tagged "Player".
-/// </summary>
 public class Equipment : MonoBehaviour, ISaveable
 {
     // STATE
@@ -17,14 +11,8 @@ public class Equipment : MonoBehaviour, ISaveable
 
     // PUBLIC
 
-    /// <summary>
-    /// Broadcasts when the items in the slots are added/removed.
-    /// </summary>
     public event Action equipmentUpdated;
 
-    /// <summary>
-    /// Return the item in the given equip location.
-    /// </summary>
     public EquipableItem GetItemInSlot(EquipLocation equipLocation)
     {
         if (!equippedItems.ContainsKey(equipLocation))
@@ -35,10 +23,6 @@ public class Equipment : MonoBehaviour, ISaveable
         return equippedItems[equipLocation];
     }
 
-    /// <summary>
-    /// Add an item to the given equip location. Do not attempt to equip to
-    /// an incompatible slot.
-    /// </summary>
     public void AddItem(EquipLocation slot, EquipableItem item)
     {
         Debug.Assert(item.GetAllowedEquipLocation() == slot);
@@ -51,9 +35,6 @@ public class Equipment : MonoBehaviour, ISaveable
         }
     }
 
-    /// <summary>
-    /// Remove the item for the given slot.
-    /// </summary>
     public void RemoveItem(EquipLocation slot)
     {
         equippedItems.Remove(slot);
@@ -90,6 +71,6 @@ public class Equipment : MonoBehaviour, ISaveable
             }
         }
     }
-    
+
 }
 
---
+++ Assets/Scripts/UI/Equipment/IItemHolder.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Allows the `ItemTooltipSpawner` to display the right information.
+/// </summary>
+public interface IItemHolder
+{
+    InventoryItem GetItem();
+}
