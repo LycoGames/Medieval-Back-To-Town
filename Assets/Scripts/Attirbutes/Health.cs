43d89b3c8bfcba8bebef1962feee7bd714329c9f
utkusever <c.utkusever@outlook.com>
2021-12-28 02:15:41 +0300

    Need Fix.

--- Assets/Scripts/Attirbutes/Health.cs
+++ Assets/Scripts/Attirbutes/Health.cs
@@ -1,14 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
-using RPG.Core;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
     [SerializeField] float regenerationPercentage = 50;
-    [SerializeField] UnityEvent onDie;
-    [SerializeField] UnityEvent takeDamage;
+    public UnityEvent onDie;
+    public UnityEvent takeDamage;
 
     LazyValue<float> healthPoints;
 
