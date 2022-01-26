e221487aed35f1d2eb98fdcaed3ec547d3b79abd
utkusever <c.utkusever@outlook.com>
2022-01-27 00:07:35 +0300

    Add: Health Text UI

---
+++ Assets/HealthDisplay.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthDisplay : MonoBehaviour
+{
+
+    Health health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Text>().text = string.Format("{0:0}/{1:0}", health.GetHealthPoints(), health.GetInitialHealth());
+    }
+
+}
