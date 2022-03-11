4838c1091ef967c43358c78e4a144ce1b53e4b33
ahmetsak-tr <ahmeterensak@outlook.com>
2022-03-11 23:14:44 +0300

    Add: Dialog Editor

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -1,23 +1,133 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue", order = 0)]
-public class Dialogue : ScriptableObject
+public class Dialogue : ScriptableObject, ISerializationCallbackReceiver
 {
     [SerializeField] private List<DialogueNode> nodes = new List<DialogueNode>();
+
+    [SerializeField] private Vector2 newNodeOffset = new Vector2(250, 0);
+
+
+    private Dictionary<string, DialogueNode> nodeLookup = new Dictionary<string, DialogueNode>();
 
 #if UNITY_EDITOR
     private void Awake()
     {
-        if (nodes.Count == 0)
-        {
-            nodes.Add(new DialogueNode());
-        }
+        OnValidate();
     }
 #endif
+
+    private void OnValidate()
+    {
+        if (nodes.Count == 0)
+        {
+            CreateNode(null);
+        }
+
+        nodeLookup.Clear();
+        foreach (DialogueNode node in GetAllNodes())
+        {
+            nodeLookup[node.name] = node;
+        }
+    }
 
     public IEnumerable<DialogueNode> GetAllNodes()
     {
         return nodes;
+    }
+
+    public DialogueNode GetRootNode()
+    {
+        return nodes[0];
+    }
+
+    public IEnumerable<DialogueNode> GetAllChildren(DialogueNode parentNode)
+    {
+        foreach (string childID in parentNode.GetChildren())
+        {
+            if (nodeLookup.ContainsKey(childID))
+            {
+                yield return nodeLookup[childID];
+            }
+        }
+    }
+
+#if UNITY_EDITOR
+    public void CreateNode(DialogueNode parent)
+    {
+        DialogueNode newNode = MakeNode(parent);
+        Undo.RegisterCreatedObjectUndo(newNode, "Created Dialogue Node");
+        Undo.RecordObject(this, "Added Dialogue Node");
+        AddNode(newNode);
+    }
+
+
+    public void DeleteNode(DialogueNode nodeToDelete)
+    {
+        Undo.RecordObject(this, "Delete Dialogue Node");
+        nodes.Remove(nodeToDelete);
+        OnValidate();
+        CleanDanglingChildren(nodeToDelete);
+        Undo.DestroyObjectImmediate(nodeToDelete);
+    }
+
+    private DialogueNode MakeNode(DialogueNode parent)
+    {
+        DialogueNode newNode = CreateInstance<DialogueNode>();
+        newNode.name = Guid.NewGuid().ToString();
+        if (parent != null)
+        {
+            parent.AddChild(newNode.name);
+            newNode.SetPlayerSpeaking(!parent.IsPlayerSpeaking());
+            newNode.SetPosition(parent.GetRect().position + newNodeOffset);
+        }
+
+        return newNode;
+    }
+
+    private void AddNode(DialogueNode newNode)
+    {
+        nodes.Add(newNode);
+        OnValidate();
+    }
+
+    private void CleanDanglingChildren(DialogueNode nodeToDelete)
+    {
+        foreach (DialogueNode node in GetAllNodes())
+        {
+            node.RemoveChild(nodeToDelete.name);
+        }
+    }
+#endif
+    // About the save a file to harddrive
+    public void OnBeforeSerialize()
+    {
+#if UNITY_EDITOR
+        if (nodes.Count == 0)
+        {
+            DialogueNode newNode = MakeNode(null);
+            AddNode(newNode);
+        }
+
+        if (AssetDatabase.GetAssetPath(this) != "")
+        {
+            foreach (DialogueNode node in GetAllNodes())
+            {
+                if (AssetDatabase.GetAssetPath(node) == "")
+                {
+                    AssetDatabase.AddObjectToAsset(node, this);
+                }
+            }
+        }
+#endif
+    }
+
+    //Load file from harddrive
+    public void OnAfterDeserialize()
+    {
     }
 }
--- Assets/Scripts/Dialogue/DialogueNode.cs
+++ Assets/Scripts/Dialogue/DialogueNode.cs
@@ -1,10 +1,72 @@
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-[System.Serializable]
-public class DialogueNode
+public class DialogueNode : ScriptableObject
 {
-    public string uniqueID;
-    public string text;
-    public string[] children;
-    public Rect position;
+    [SerializeField] private bool isPlayerSpeaking = false;
+    [SerializeField] private string text;
+    [SerializeField] private List<string> children = new List<string>();
+    [SerializeField] private Rect rect = new Rect(0, 0, 200, 100);
+
+    public Rect GetRect()
+    {
+        return rect;
+    }
+
+    public string GetText()
+    {
+        return text;
+    }
+
+    public List<string> GetChildren()
+    {
+        return children;
+    }
+
+    public bool IsPlayerSpeaking()
+    {
+        return isPlayerSpeaking;
+    }
+
+#if UNITY_EDITOR
+    public void SetPosition(Vector2 newPosition)
+    {
+        Undo.RecordObject(this, "Move Dialogue Node");
+        rect.position = newPosition;
+        EditorUtility.SetDirty(this);
+    }
+
+    public void SetText(string newText)
+    {
+        Undo.RecordObject(this, "Move Dialogue Node");
+        if (newText != text)
+        {
+            Undo.RecordObject(this, "Update Dialogue Text");
+            text = newText;
+            EditorUtility.SetDirty(this);
+        }
+    }
+
+    public void AddChild(string childID)
+    {
+        Undo.RecordObject(this, "Add Dialogue Link");
+        children.Add(childID);
+        EditorUtility.SetDirty(this);
+    }
+
+    public void RemoveChild(string childID)
+    {
+        Undo.RecordObject(this, "Remove Add Dialogue Link");
+        children.Remove(childID);
+        EditorUtility.SetDirty(this);
+    }
+
+    public void SetPlayerSpeaking(bool newIsPlayerSpeaking)
+    {
+        Undo.RecordObject(this, "Change Dialog Speaker");
+        isPlayerSpeaking = newIsPlayerSpeaking;
+        EditorUtility.SetDirty(this);
+    }
+#endif
 }
