9ed8d5c5802d0019e3ade71fbcab66a674c428b5
ahmetsak-tr <ahmeterensak@outlook.com>
2022-04-11 23:40:09 +0300

    Update: Merge error fixes

--- Assets/Scripts/Inventories/ActionItem.cs
+++ Assets/Scripts/Inventories/ActionItem.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 [CreateAssetMenu(menuName = ("Inventory/Action Item"))]
 public class ActionItem : InventoryItem
 {
-    [SerializeField] private bool consumable=false;
+    [SerializeField] private bool consumable = false;
+
     public virtual void Use(GameObject user)
     {
-        Debug.Log("Usin Action: "+this);
+        Debug.Log("Usin Action: " + this);
     }
 
     public bool isConsumable()
