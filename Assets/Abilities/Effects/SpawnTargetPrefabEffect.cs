2356a727b58a3dd22dc3cf533250f8c1a6309ebd
utkusever <c.utkusever@outlook.com>
2022-04-24 20:47:27 +0300

    Add: new Skill, Cooldown, custom color

--- Assets/Abilities/Effects/SpawnTargetPrefabEffect.cs
+++ Assets/Abilities/Effects/SpawnTargetPrefabEffect.cs
@@ -9,6 +9,11 @@ public class SpawnTargetPrefabEffect : EffectStrategy
     [SerializeField] Transform prefabToSpawn;
     [SerializeField] float destroyDelay = -1;
     [SerializeField] bool shouldSpawnOnPoint;
+    [SerializeField] bool shouldSpawnOnHand;
+    [SerializeField] bool staticSpawnPoint;
+    GameObject player;
+
+    Vector3 posX;
 
 
     public override void StartEffect(AbilityData data, Action finished)
@@ -16,20 +21,51 @@ public class SpawnTargetPrefabEffect : EffectStrategy
         data.StartCoroutine(Effect(data, finished));
     }
 
+    private IEnumerator UpdateX()
+    {
+        while (true)
+        {
+            Debug.Log("konum " + posX);
+            yield return null;
+        }
+    }
+
     private IEnumerator Effect(AbilityData data, Action finished)
     {
-        Transform instance = Instantiate(prefabToSpawn);
+
+        // Transform instance = Instantiate(prefabToSpawn);
+        Transform instance;
         if (shouldSpawnOnPoint)
         {
-            instance.position = GameObject.Find("Bow pivot").transform.position;
-            instance.forward = data.GetUser().transform.up;
+            instance = Instantiate(prefabToSpawn, data.GetUser().GetComponent<StateMachine>().GetHeadTransform());
+            // instance.forward = data.GetUser().transform.forward;
+
         }
+
+        else if (shouldSpawnOnHand)
+        {
+            if (staticSpawnPoint)
+            {
+                Debug.Log("asdffsda");
+                instance = Instantiate(prefabToSpawn);
+                instance.position = data.GetUser().GetComponent<StateMachine>().GetLeftHandTransform().position;
+            }
+            else { instance = Instantiate(prefabToSpawn, data.GetUser().GetComponent<StateMachine>().GetLeftHandTransform()); Debug.Log("aaaaa"); }
+
+            // instance.position = data.GetUser().gameObject.transform.Find("arissa model").transform.Find("arissa:Hips").transform.Find("arissa:Spine").transform.Find("arissa:Spine1").transform.Find("arissa:Spine2").transform.Find("arissa:LeftShoulder").transform.Find("arissa:LeftArm").transform.Find("arissa:LeftForeArm").transform.Find("arissa:LeftHand").transform.position;
+            // instance.position = data.GetUser().GetComponent<InstantiatePoints>().GetLeftHandTransform().position;
+            //instance.forward = data.GetUser().transform.forward;
+        }
+
         else
         {
-            instance.position = data.GetTargetedPoint();
+            instance = Instantiate(prefabToSpawn, data.GetTargetedPoint(), Quaternion.identity);
+            //instance.position = data.GetTargetedPoint();
         }
+
         if (destroyDelay > 0)
         {
+
             yield return new WaitForSeconds(destroyDelay);
             Destroy(instance.gameObject);
         }
---
+++ Assets/InstantiatePoints.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiatePoints : MonoBehaviour
+{
+
+    [SerializeField] private Transform rightHandTransform = null;
+    public Transform GetRightHandTransform() { return rightHandTransform; }
+    [SerializeField] private Transform leftHandTransform = null;
+    public Transform GetLeftHandTransform() { return leftHandTransform; }
+    [SerializeField] private Transform head = null;
+    public Transform GetHeadTransform() { return head; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("head" + head.transform.position);
+    }
+}
