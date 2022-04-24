18aa0d2fcc836d9a184034d864bc7ad24e72fcf8
utkusever <c.utkusever@outlook.com>
2022-04-24 22:39:56 +0300

    Add: new Skill and Anim Triggers

--- Assets/Abilities/Effects/SpawnTargetPrefabEffect.cs
+++ Assets/Abilities/Effects/SpawnTargetPrefabEffect.cs
@@ -21,15 +21,6 @@ public class SpawnTargetPrefabEffect : EffectStrategy
         data.StartCoroutine(Effect(data, finished));
     }
 
-    private IEnumerator UpdateX()
-    {
-        while (true)
-        {
-            Debug.Log("konum " + posX);
-            yield return null;
-        }
-    }
-
     private IEnumerator Effect(AbilityData data, Action finished)
     {
 
@@ -46,11 +37,12 @@ public class SpawnTargetPrefabEffect : EffectStrategy
         {
             if (staticSpawnPoint)
             {
-                Debug.Log("asdffsda");
                 instance = Instantiate(prefabToSpawn);
                 instance.position = data.GetUser().GetComponent<StateMachine>().GetLeftHandTransform().position;
+                instance.rotation = data.GetUser().GetComponent<StateMachine>().GetLeftHandTransform().rotation;
+
             }
-            else { instance = Instantiate(prefabToSpawn, data.GetUser().GetComponent<StateMachine>().GetLeftHandTransform()); Debug.Log("aaaaa"); }
+            else { instance = Instantiate(prefabToSpawn, data.GetUser().GetComponent<StateMachine>().GetLeftHandTransform()); }
 
             // instance.position = data.GetUser().gameObject.transform.Find("arissa model").transform.Find("arissa:Hips").transform.Find("arissa:Spine").transform.Find("arissa:Spine1").transform.Find("arissa:Spine2").transform.Find("arissa:LeftShoulder").transform.Find("arissa:LeftArm").transform.Find("arissa:LeftForeArm").transform.Find("arissa:LeftHand").transform.position;
             // instance.position = data.GetUser().GetComponent<InstantiatePoints>().GetLeftHandTransform().position;
