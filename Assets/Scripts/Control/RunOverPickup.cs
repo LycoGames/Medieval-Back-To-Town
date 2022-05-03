86c950b08ed51fd478a12fb58644f953f638276f
utkusever <c.utkusever@outlook.com>
2022-05-03 19:07:49 +0300

    Add: Collect Sound effect

--- Assets/Scripts/Control/RunOverPickup.cs
+++ Assets/Scripts/Control/RunOverPickup.cs
@@ -6,12 +6,17 @@ using UnityEngine;
 [RequireComponent(typeof(Pickup))]
 public class RunOverPickup : MonoBehaviour
 {
+    AudioSource audioSource;
+    [SerializeField] AudioClip audioClip;
     private void OnTriggerEnter(Collider other)
     {
         var player = GameObject.FindGameObjectWithTag("Player");
         if (other.gameObject == player)
         {
             GetComponent<Pickup>().PickupItem();
+            audioSource = player.GetComponent<AudioSource>();
+            //audioSource.clip = audioClip;
+            audioSource.PlayOneShot(audioClip);
             //TODO playoneshot audio
         }
     }
