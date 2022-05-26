2fee350d597a1e9732ac93701cb86c04f6d6e448
utkusever <c.utkusever@outlook.com>
2022-05-27 00:41:54 +0300

    Add: Drop libraries for mobs Update: Collect Text

--- Assets/Scripts/UI/HUD/ItemPickupAlert.cs
+++ Assets/Scripts/UI/HUD/ItemPickupAlert.cs
@@ -5,11 +5,26 @@ using UnityEngine.UI;
 
 public class ItemPickupAlert : MonoBehaviour
 {
+    [SerializeField] GameObject text;
 
+    float timer = 0;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= 3f)
+        {
+            gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            //text.SetActive(false);
+        }
+    }
     public void SetText(string name)
     {
+     //   text.SetActive(true);
+        timer = 0;
+        gameObject.transform.GetChild(0).gameObject.SetActive(true);
         string itemName = name;
-        GetComponent<Text>().text = string.Format("Edindin: " + itemName);
+        text.GetComponent<Text>().text = string.Format("Edindin: " + itemName);
     }
 
 }
